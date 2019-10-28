620f2fd1026c39f82e887f02847fe5529214325f
Luisb <luis.brito@siwgroup.com>
2019-10-28 17:19:44 -0300

    first commit

---
+++ src/MemeGenerator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MemeGenerator extends React.Component {
+    constructor() {
+        super();
+        this.state = {} 
+    }
+    render() {
+        return <h1>MEME GENERATOR SECTION</h1>
+    }
+}
+
+export default MemeGenerator;
